2d9471f34aeb24fbb0adb7b8ea60a951b4d811a1
vaki611 <vasilija.rnic@gmail.com>
2023-12-25 12:00:24 +0100

    autentifikacija

--- booking/app/Http/Controllers/ReservationController.php
+++ booking/app/Http/Controllers/ReservationController.php
@@ -6,6 +6,7 @@ use App\Models\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Resources\ReservationResource;
 use App\Http\Resources\ReservationCollection;
+use Validator;
 class ReservationController extends Controller
 {
     /**
@@ -31,8 +32,32 @@ class ReservationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        $validator = Validator::make($request->all(), [
+            'apartment_id' => 'required',
+            'client_id' => 'required',
+            'check_in_date' => 'required|date',
+            'check_out_date' => 'required|date|after:check_in_date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        // Add additional logic if needed, e.g., checking if the apartment and client exist
+
+        $reservation = Reservation::create([
+            'apartment_id' => $request->apartment_id,
+            'client_id' => $request->client_id,
+            'check_in_date' => $request->check_in_date,
+            'check_out_date' => $request->check_out_date,
+        ]);
+
+        return response()->json(['Reservation created successfully.',new ReservationResource($reservation)]);
     }
+    
+    
+    
 
     /**
      * Display the specified resource.
@@ -55,7 +80,26 @@ class ReservationController extends Controller
      */
     public function update(Request $request, Reservation $reservation)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'apartment_id' => 'required',
+            'client_id' => 'required',
+            'check_in_date' => 'required|date',
+            'check_out_date' => 'required|date|after:check_in_date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $reservation->apartment_id = $request->apartment_id;
+        $reservation->client_id = $request->client_id;
+        $reservation->check_in_date = $request->check_in_date;
+        $reservation->check_out_date = $request->check_out_date;
+
+        $reservation->save();
+        return response()->json(['Reservation updated sucessfully.', new ReservationResource($reservation)]);
+
+
     }
 
     /**
@@ -63,6 +107,7 @@ class ReservationController extends Controller
      */
     public function destroy(Reservation $reservation)
     {
-        //
+        $reservation->delete();
+        return response()->json('Reservation deleted successfully');
     }
 }
