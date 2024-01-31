a2f17dde3c12bbdd6209a6eaad5cb2d84e009c71
Milenica88 <paripovicmilena@gmail.com>
2024-01-31 22:27:01 +0100

    sredjivanje stranice

--- react/reactapp/src/komponente/pretrazivanje/Pretrazivanje.jsx
+++ react/reactapp/src/komponente/pretrazivanje/Pretrazivanje.jsx
@@ -2,8 +2,9 @@ import "./pretrazivanje.css"
 
 const Pretrazivanje = () => {
   return (
+    <div className="search">
     <div className="searchItem">
-    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+    <img src="/slike/pretrazivanje/6.webp"
     alt="" 
     className="siImg" />
      <div className="siDesc">
@@ -33,6 +34,101 @@ const Pretrazivanje = () => {
     </div>
   </div>
 </div>
+<div className="searchItem">
+    <img src="/slike/pretrazivanje/1.jpg"
+    alt="" 
+    className="siImg" />
+     <div className="siDesc">
+    <h1 className="siTitle">Bresidence Apartments sa privatnim parkingom</h1>
+    <span className="siDistance">1km od centra</span>
+    <span className="siTaxiOp">Besplatan prevoz od/do aerodroma</span>
+    <span className="siSubtitle">
+      Apartman
+    </span>
+    <span className="siFeatures">
+      Čitav studio • 2 kupatila • 70m² 2 kreveta
+    </span>
+    <span className="siCancelOp">Besplatno otkazivanje</span>
+    <span className="siCancelOpSubtitle">
+        Možete otkazati kasnije, zato iskoristite ovu odličnu cenu danas!
+    </span>
+  </div>
+  <div className="siDetails">
+    <div className="siRating">
+        <span>Izuzetan</span>
+        <button>9.7</button>
+    </div>
+    <div className="siDetailTexts">
+      <span className="siPrice">17.955 RSD</span>
+      <span className="siTaxOp">+ takse i naknade</span>
+      <button className="siCheckButton">Prikaži raspoloživost</button>
+    </div>
+  </div>
+</div>
+<div className="searchItem">
+    <img src="/slike/pretrazivanje/2.jpg"
+    alt="" 
+    className="siImg" />
+     <div className="siDesc">
+    <h1 className="siTitle">Villa pod Gradom</h1>
+    <span className="siDistance">700m od centra</span>
+    <span className="siTaxiOp">Besplatan prevoz od/do autobuske</span>
+    <span className="siSubtitle">
+      Studio Apartman sa klimom
+    </span>
+    <span className="siFeatures">
+      Čitav studio • 1 kupatilo • 20m² 1 krevet
+    </span>
+    <span className="siCancelOp">Besplatno otkazivanje</span>
+    <span className="siCancelOpSubtitle">
+        Možete otkazati kasnije, zato iskoristite ovu odličnu cenu danas!
+    </span>
+  </div>
+  <div className="siDetails">
+    <div className="siRating">
+        <span>Dobar</span>
+        <button>7.1</button>
+    </div>
+    <div className="siDetailTexts">
+      <span className="siPrice">6.000 RSD</span>
+      <span className="siTaxOp">+ takse i naknade</span>
+      <button className="siCheckButton">Prikaži raspoloživost</button>
+    </div>
+  </div>
+</div>
+<div className="searchItem">
+    <img src="/slike/pretrazivanje/4.jpg"
+    alt="" 
+    className="siImg" />
+     <div className="siDesc">
+    <h1 className="siTitle">Old Town Theatre Staircase Apartments</h1>
+    <span className="siDistance">2km od centra</span>
+    <span className="siTaxiOp">Besplatan prevoz od/do aerodroma</span>
+    <span className="siSubtitle">
+      Studio Apartman bez klime
+    </span>
+    <span className="siFeatures">
+      Čitav studio • 3 kupatila • 120m² 3 krevet
+    </span>
+    <span className="siCancelOp">Besplatno otkazivanje</span>
+    <span className="siCancelOpSubtitle">
+        Možete otkazati kasnije, zato iskoristite ovu odličnu cenu danas!
+    </span>
+  </div>
+  <div className="siDetails">
+    <div className="siRating">
+        <span>Veoma dobar</span>
+        <button>8.0</button>
+    </div>
+    <div className="siDetailTexts">
+      <span className="siPrice">20.000 RSD</span>
+      <span className="siTaxOp">+ takse i naknade</span>
+      <button className="siCheckButton">Prikaži raspoloživost</button>
+    </div>
+  </div>
+</div>
+</div>
+
   )
 }
 
