5ad0fd3c0f8f9309ff60339a5c11ae8848f5982c
Milenica88 <paripovicmilena@gmail.com>
2024-02-09 19:27:59 +0100

    podaci iz baze

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -1,5 +1,5 @@
 import Hotel from "../models/Hotel.js"
-
+import Room from "../models/Room.js";
 export const createHotel=async(req,res,next)=>{
     const newHotel=new Hotel(req.body)
 
@@ -39,14 +39,18 @@ export const getHotel=async(req,res,next)=>{
     }
 }
 
-export const getHotels=async(req,res,next)=>{
+export const getHotels = async (req, res, next) => {
+    const { min, max, limit, ...others } = req.query;
     try {
-        const hotels=await Hotel.find()
-        res.status(200).json(hotels)
-       } catch (err) {
-        next(err)
-       }
-}
+      const hotels = await Hotel.find({
+        ...others,
+        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      }).limit(+limit);
+      res.status(200).json(hotels);
+    } catch (err) {
+      next(err);
+    }
+  };
 
 export const countByCity=async(req,res,next)=>{
     const cities=req.query.cities.split(",")
