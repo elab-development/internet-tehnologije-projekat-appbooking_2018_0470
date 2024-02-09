caf67c396a23390f1d243ba49a494dd136025b3b
Milenica88 <paripovicmilena@gmail.com>
2024-02-09 12:20:26 +0100

    modeli

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -11,7 +11,7 @@ export const createHotel=async(req,res,next)=>{
     }
 }
 
-export const updatedHotel=async(req,res,next)=>{
+export const updateHotel=async(req,res,next)=>{
     try {
         const updatedHotel=await Hotel.findByIdAndUpdate(req.params.id,{$set: req.body},{new:true})
         res.status(200).json(updatedHotel)
--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updatedHotel } from "../controllers/hotel.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router=express.Router();
@@ -8,7 +8,7 @@ const router=express.Router();
 //CREATE
 router.post("/",verifyAdmin,createHotel);
 //UPDATE
-router.put("/:id",verifyAdmin,updatedHotel)
+router.put("/:id",verifyAdmin,updateHotel)
 //DELETE
 router.delete("/:id",verifyAdmin,deleteHotel)
 //GET
