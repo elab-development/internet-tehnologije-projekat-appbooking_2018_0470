7da07239582d490fb30dc162dbebe172725fa546
Milenica88 <paripovicmilena@gmail.com>
2024-02-09 14:38:23 +0100

    customHook

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router=express.Router();
@@ -8,12 +8,14 @@ const router=express.Router();
 //CREATE
 router.post("/",verifyAdmin,createHotel);
 //UPDATE
-router.put("/:id",verifyAdmin,updateHotel)
+router.put("/:id",verifyAdmin,updateHotel);
 //DELETE
-router.delete("/:id",verifyAdmin,deleteHotel)
+router.delete("/:id",verifyAdmin,deleteHotel);
 //GET
-router.get("/:id",getHotel)
+router.get("/find/:id",getHotel);
 //GET ALL
-router.get("/",getHotels)
+router.get("/",getHotels);
+router.get("/countByCity",countByCity);
+router.get("/countByType",countByType);
 
 export default router
