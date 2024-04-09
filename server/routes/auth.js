037e66acae77acd74d72757be0e37aa58d65de30
Milenica88 <paripovicmilena@gmail.com>
2024-04-09 18:28:30 +0200

    helmet

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { login, register,logout } from "../controllers/auth.js";
+import { login, register, logout } from "../controllers/auth.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/register",register)
-router.post("/login",login)
-router.post('/logout', logout);
-export default router
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+export default router;
--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,22 +1,31 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotelRooms, getHotels, updateHotel } from "../controllers/hotel.js";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
-const router=express.Router();
+const router = express.Router();
 
 //CREATE
-router.post("/",verifyAdmin,createHotel);
+router.post("/", verifyAdmin, createHotel);
 //UPDATE
-router.put("/:id",verifyAdmin,updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id",verifyAdmin,deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get("/find/:id",getHotel);
+router.get("/find/:id", getHotel);
 //GET ALL
-router.get("/",getHotels);
-router.get("/countByCity",countByCity);
-router.get("/countByType",countByType);
-router.get("/room/:id",getHotelRooms);
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
-export default router
+export default router;
